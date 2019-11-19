306db0d4dd1ffa849940ea6f0a24c67f3a475cb3
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2019-11-19 21:43:57 +0100

    change text and glow around circle

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 window.addEventListener('load', loadSite)
 
 function loadSite() {
- 
-rotateText()
+
+//rotateText()
+
+
+const startRotateText = document.querySelector('.inner-circle')
+startRotateText.addEventListener('click', rotateTextAroundCircle )
+
 
 const menu = document.querySelector('.dropdown-icon')
 menu.addEventListener('click', showMenu )
 }
 
 
-function rotateText() {
+function rotateTextAroundCircle() {
+console.log('rotate')
+// Code for Safari
+document.querySelectorAll(".outercircle").style.WebkitTransform = "rotate(20deg)"; 
+// Code for IE9
+document.querySelectorAll(".outercircle").style.msTransform = "rotate(20deg)"; 
+// Standard syntax
+document.querySelectorAll(".outercircle").style.transform = "rotate(20deg)"; 
+  }
+
+
+
+/* function rotateText() {
     const span = document.querySelector('.hello-text')  
     span.setAttribute( 'transform','rotate(180deg)')
     console.log('hej')
-} 
+}  */
 
 
 let show
