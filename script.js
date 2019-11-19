a3e3047bfae5c4111c854c5b3271c17b2302eb37
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2019-11-19 15:33:23 +0100

    change font family

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 window.addEventListener('load', loadSite)
 
 function loadSite() {
+ 
+rotateText()
 
 const menu = document.querySelector('.dropdown-icon')
 menu.addEventListener('click', showMenu )
 }
+
+
+function rotateText() {
+    const span = document.querySelector('.hello-text')  
+    span.setAttribute( 'transform','rotate(180deg)')
+    console.log('hej')
+} 
+
+
 let show
 function showMenu() {
     if(show)
     {const menuItems = document.querySelectorAll('.show')
 for(const item of menuItems) {
     item.style.opacity = "1"
-}}
+    }
+}
  else{
     const menuItems = document.querySelectorAll('.show')
     for(const item of menuItems) {
